a4ac956ab7c40982b00acab7990e985b0266aa42
nelisabe <aleksandr.engoyan@gmail.com>
2020-12-22 18:00:22 +0300

    add wordpress (working, but not connect to database)

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/18 16:33:40 by nelisabe          #+#    #+#              #
-#    Updated: 2020/12/20 23:07:23 by nelisabe         ###   ########.fr        #
+#    Updated: 2020/12/22 18:00:00 by nelisabe         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,8 +17,10 @@ eval $(minikube docker-env)
 docker build -t ftps_image ./srcs/ftps/
 docker build -t nginx_image ./srcs/nginx/
 docker build -t mysql_image ./srcs/mysql/
+docker build -t wordpress_image ./srcs/wordpress/
 
 kubectl apply -f ./srcs/configs/
 kubectl apply -f ./srcs/ftps/configs/
 kubectl apply -f ./srcs/nginx/configs/
 kubectl apply -f ./srcs/mysql/configs/
+kubectl apply -f ./srcs/wordpress/configs/
--- srcs/wordpress/setup_wordpress.sh
+++ srcs/wordpress/setup_wordpress.sh
@@ -6,7 +6,8 @@
 #    By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/20 23:18:29 by nelisabe          #+#    #+#              #
-#    Updated: 2020/12/20 23:18:30 by nelisabe         ###   ########.fr        #
+#    Updated: 2020/12/22 15:28:45 by nelisabe         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+supervisord -c supervisord.conf
