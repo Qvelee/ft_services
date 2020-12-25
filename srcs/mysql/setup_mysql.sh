084b58e6f4cb0f6c4cfda4d820c55e23e517b8da
nelisabe <aleksandr.engoyan@gmail.com>
2020-12-25 20:02:15 +0300

    oma works troght proxy, wp have starting database, users

--- srcs/mysql/setup_mysql.sh
+++ srcs/mysql/setup_mysql.sh
@@ -6,14 +6,15 @@
 #    By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/20 17:42:55 by nelisabe          #+#    #+#              #
-#    Updated: 2020/12/24 17:18:51 by nelisabe         ###   ########.fr        #
+#    Updated: 2020/12/25 19:57:54 by nelisabe         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 mysql_install_db --user=mysql --datadir=/var/lib/mysql	# initializes the MariaDB data directory and creates the system tables in the mysql database
 rc-update add mariadb default							# adding mariadb service to default alpine linux runlevel
 openrc default											# swap current runlevel to default (here is sending services)
-cat ./mysql_config | mysql								# send commands to mysql to create wordpress database
+mysql < ./mysql_config									# send commands to mysql to create wordpress database
+mysql wordpress_database < ./wordpress_database.sql		# send wordpress database (with users)
 rc-service mariadb stop									# stop mysql daemon
 cp ./my.cnf /etc/
 /usr/bin/mysqld_safe --datadir=/var/lib/mysql			# run mysql like process
